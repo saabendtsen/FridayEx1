0c2b1e06db6244660a180665aad6255dbe925a97
Søren Aagaard Bendtsen <soren@tech-logic.dk>
2021-08-28 11:57:57 +0200

    first test

---
+++ src/test/java/model/DataMapperTest.java
@@ -0,0 +1,14 @@
+package model;
+
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DataMapperTest {
+
+    @Test
+    public void createPerson() {
+    assertTrue(true);
+    }
+
+}
